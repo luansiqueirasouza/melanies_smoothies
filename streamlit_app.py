33383e39dbeae253d529d74809044b00a12294a0
Luan Siqueira Souza <luansiqueirasouza@gmail.com>
2026-05-25 19:12:06 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,10 +55,10 @@ if ingredients_list:
         #st.stop()
         #st.write(my_insert_stmt)
       
-        time_to_insert = st.button('Submit Order')
-        if time_to_insert:
-            session.sql(my_insert_stmt).collect()
-            st.success(f'Your Smoothie is ordered, {customer_name}!', icon="✅")
+time_to_insert = st.button('Submit Order')
+if time_to_insert:
+  session.sql(my_insert_stmt).collect()
+  st.success(f'Your Smoothie is ordered, {customer_name}!', icon="✅")
 
 
 # st.text(smoothiefroot_response.json(), use_container_width=True)
